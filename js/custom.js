31deda41b5724e140e2157f91a20022c9ed7a762
Tony Tambe <tambe257@gmail.com>
2015-10-02 13:48:39 -0400

    CSS updates and added social icons

--- js/custom.js
+++ js/custom.js
@@ -1,26 +1,3 @@
-/* Fade in elements on scroll */
-
-$(function() {
-    $(window).scroll( function(){
-        $('.fadeInBlock').each( function(i){
-            
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            /* Adjust the "50" to either have a delay or that the content starts fading a bit before you reach it  */
-            bottom_of_window = bottom_of_window + 20;  
-          
-            if( bottom_of_window > bottom_of_object ){
-                
-                $(this).animate({'opacity':'1'},800);
-                    
-            }
-        }); 
-    
-    });
-});
-
-
 /* Fade in Welcome Text on load */
 
 $('.welcome-text').hide().fadeIn(1000);
@@ -34,17 +11,17 @@ $('.project').hide().fadeIn(1000);
 /* Animate social icons in the bottom section on scroll */
 $(function() {
     $(window).scroll( function(){
-        $('#bottom').each( function(i){
+        $('.social').each( function(i){
             
             var bottom_of_object = $(this).position().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
             /* Adjust the "50" to either have a delay or that the content starts fading a bit before you reach it  */
-            bottom_of_window = bottom_of_window + 20;  
+            bottom_of_window = bottom_of_window + 0;  
           
             if( bottom_of_window > bottom_of_object ){
                 
-                $('#twitter').addClass('animate');
+                $('#instagram').addClass('animate');
                 $('#mail').addClass('animate');
                     
             }
